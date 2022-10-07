85352878a256b5655b2359579ef1de90359f8fac
drorsal84 <drorsal84@gmail.com>
2022-10-07 20:51:41 +0300

    Fixed error rendering issue

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -65,12 +65,12 @@ const sendErrorProd = (err, req, res) => {
 
   // B) RENDERED WEBSITE
   // A) Operational, trusted error: send message to client
-  if (err.isOperational) {
-    return res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message,
-    });
-  }
+  // if (err.isOperational) {
+  //   return res.status(err.statusCode).json({
+  //     status: err.status,
+  //     message: err.message,
+  //   });
+  // }
   // B) Programming or other unknown error: don't leak error details
   // 1) Log error
   //console.error('ERROR 💥', err);
