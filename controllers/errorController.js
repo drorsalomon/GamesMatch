af9a9c061caffd57e4e879361bb53346fd9d2e54
drorsal84 <drorsal84@gmail.com>
2022-10-17 11:00:08 +0300

    Fixed error controller

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -15,25 +15,6 @@ const handleEmailExistsError = () => new AppError('Email already connected to an
 const passwordsNotTheSame = () => new AppError('Password and confirm password fields are not the same!', 401);
 
 const sendErrorDev = (err, req, res) => {
-  // API
-  if (req.originalUrl.startsWith('/api')) {
-    if (err.name === 'ValidationError' && err.message.includes('passwordConfirm')) {
-      err.message = err.message.substring(41, 250);
-    }
-
-    if (err.name === 'MongoError' && err.message.includes('email_1 dup key:')) {
-      err = handleEmailExistsError();
-    }
-    return res.status(err.statusCode).json({
-      status: err.status,
-      error: err,
-      message: err.message,
-      stack: err.stack,
-    });
-  }
-
-  // B) RENDERED WEBSITE
-  if (err.name === 'JsonWebTokenError') err = handleJWTError();
   console.error('ERROR 💥', err);
   console.log(err.message);
   console.log(err.name);
@@ -44,28 +25,17 @@ const sendErrorDev = (err, req, res) => {
 };
 
 const sendErrorProd = (err, req, res) => {
-  // API
   // If the error is operational (caused by the app logic)... the 'isOperational' variable is specified in the appError class
-  if (req.originalUrl.startsWith('/api')) {
-    // A) Operational, trusted error: send message to client
-    if (err.isOperational) {
-      return res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message,
-      });
-    }
-    // If the error is a bug in the app we want to log the error for review and send the user a generic message
-    console.error('ERROR 💥', err);
-    // 2) Send generic message
-    return res.status(500).json({
-      status: 'error',
-      message: 'Something went very wrong!',
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
+    return res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
     });
   }
-
-  // B) RENDERED WEBSITE
-  //console.error('ERROR 💥', err);
-  // 2) Send generic message
+  // If the error is a bug in the app we want to log the error for review and send the user a generic message
+  // Send generic message
+  console.error('ERROR 💥', err);
   return res.status(err.statusCode).render('error', {
     title: 'Something went wrong!',
     msg: err.message,
@@ -75,8 +45,6 @@ const sendErrorProd = (err, req, res) => {
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-  console.log(err.name);
-  console.log(err);
 
   // If we are in development mode get as much info about the error
   if (process.env.NODE_ENV === 'development') {
